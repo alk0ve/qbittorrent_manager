700f40c8d7cf204d969b1a6575c06f8eb6a63084
Alexei Kovelman <alexei.kovelman@gmail.com>
2020-04-20 10:20:15 +0300

    Add clearer prints

--- qbitman.py
+++ qbitman.py
@@ -8,6 +8,7 @@ import traceback
 SEEDING_RATIO_THRESHOLD = 2.0
 LOG_FILE_NAME = 'qbitman_log.txt'
 LOG_FILE_PATH = os.path.join(os.path.split(__file__)[0], LOG_FILE_NAME)
+HOST = 'localhost:8080'
 
 
 # redirect prints and exceptions
@@ -17,10 +18,10 @@ sys.stderr = open(LOG_FILE_PATH, 'w')
 
 def main():
     print("Starting at %s" % time.ctime())
-    qbt_client = qbittorrentapi.Client(host='localhost:8080')
+    qbt_client = qbittorrentapi.Client(host=HOST)
     qbt_client.auth_log_in() # using empty password
 
-    print("Connected")
+    print("Connected to qBitTorrent")
 
     # stop any torrent that passes the seeding ratio threshold
     for torrent in qbt_client.torrents_info():
@@ -31,11 +32,12 @@ def main():
     # close the program if everything stopped or there are no torrents
     for torrent in qbt_client.torrents_info():
         if torrent.state != 'pausedUP':
-            print("Still active: %s" % torrent.name)
+            print("Torrent still active: %s" % torrent.name)
+            print("Will not shut down qBitTorrent")
             break
     else:
         # no break, i.e. empty list or everything passed 2.0 seeding ratio
-        print("Shutting down")
+        print("No active torrents, shutting down qBitTorrent")
         qbt_client.app.shutdown
 
     print("Finished at %s" % time.ctime())
